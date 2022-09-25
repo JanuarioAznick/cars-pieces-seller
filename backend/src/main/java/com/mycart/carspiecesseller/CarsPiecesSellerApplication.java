cf16ef3084eaa9293f89ee13a75aa6f1422010ad
JanuarioAznick <januario-jose@outlook.com>
2022-09-26 00:36:49 +0200

    Project created

---
+++ backend/src/main/java/com/mycart/carspiecesseller/CarsPiecesSellerApplication.java
@@ -0,0 +1,13 @@
+package com.mycart.carspiecesseller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsPiecesSellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsPiecesSellerApplication.class, args);
+	}
+
+}
